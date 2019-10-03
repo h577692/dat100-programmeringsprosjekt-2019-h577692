c3be3ead2ae460e89a4bf2ea01f33a758554aafa
h241566 <h241566@stud.hvl.no>
2019-10-03 17:46:50 +0200

    Karius og baktus

--- src/no/hvl/dat100ptc/oppgave1/TestKlasse.java
+++ src/no/hvl/dat100ptc/oppgave1/TestKlasse.java
@@ -1,5 +1,11 @@
 package no.hvl.dat100ptc.oppgave1;
 
+public class karius {
+    public static void baktus(karius) {
+/*        TODO Karius og baktus*/
+    }
+}
+
 public class TestKlasse {
     public static void main(String[] args) {
         System.out.println("test");
