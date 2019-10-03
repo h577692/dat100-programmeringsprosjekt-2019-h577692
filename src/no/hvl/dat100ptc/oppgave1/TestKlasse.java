26d956db2e9c9ae4e8a7b37b221bafe72cf3cd09
haggbart <mr.haggbart@gmail.com>
2019-10-03 17:58:41 +0200

    baktus overload metode

--- src/no/hvl/dat100ptc/oppgave1/TestKlasse.java
+++ src/no/hvl/dat100ptc/oppgave1/TestKlasse.java
@@ -3,11 +3,18 @@ package no.hvl.dat100ptc.oppgave1;
 class karius {
     public static void baktus() {
 /*        TODO Karius og baktus*/
+        System.out.println("blir dette printet?");
+    }
+
+    public static void baktus(String text) {
+        System.out.println("her er teksten: " + text);
     }
 }
 
+
 public class TestKlasse {
     public static void main(String[] args) {
-        System.out.println("test");
+        karius.baktus();
+        karius.baktus("hoi hoi hoi");
     }
 }
