66cb1e1de3915a07e453b1aeabe59db2f5623b67
haggbart <mr.haggbart@gmail.com>
2019-10-07 13:57:32 +0200

    delvis oppgave1

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -4,73 +4,68 @@ import no.hvl.dat100ptc.TODO;
 
 public class GPSPoint {
 
-	// TODO - objektvariable
+	private int time;
+	private double latitude;
+	private double longitude;
+	private double elevation;
 		
 	public GPSPoint(int time, double latitude, double longitude, double elevation) {
 
 		// TODO - konstruktur
-		// test commit
-
-		throw new UnsupportedOperationException(TODO.construtor("GPSPoint"));
-
+		this.time = time;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.elevation = elevation;
 	}
 
 	// TODO - get/set metoder
 	public int getTime() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		return time;
 	}
 
 	public void setTime(int time) {
-				
-		throw new UnsupportedOperationException(TODO.method());
-
+		this.time = time;
 	}
 
 	public double getLatitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		return latitude;
 	}
 
 	public void setLatitude(double latitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		this.latitude = latitude;
 	}
 
 	public double getLongitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		return longitude;
 	}
 
 	public void setLongitude(double longitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		this.longitude = longitude;
 	}
 
 	public double getElevation() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		return elevation;
 	}
 
 	public void setElevation(double elevation) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		this.elevation = elevation;
 	}
-	
+
+	@Override
 	public String toString() {
-		
+
+		/*
+		Gjør ferdig implementasjonen av toString()-metoden som returnerer en strengrepresentasjon av et
+		GPSPoint-objekt på formen:
+		1 (2.0,3.0) 5.0\n
+		der 1 er tiden, (2.0,3.0) er (breddegrad,lengdegrad) og 5.0 er høyden.
+		Test implementasjonen ved å bruke enhetstestene.
+		 */
 		String str;
 		
 		// TODO - start
 
-		throw new UnsupportedOperationException(TODO.method());
+		return "hei";
 
 		// TODO - slutt
 		
